48527224bddb1390d0c31265c802b73060d2d56c
w0ckeezz <39992389+w0ckeezz@users.noreply.github.com>
2018-12-20 02:03:14 +0300

    getLengthOfList function
    getLengthOfList function added

--- achiearchi.sol
+++ achiearchi.sol
@@ -16,8 +16,10 @@ contract AchieArchi {
     
     function addAchievement(string memory _title, string memory _description) public {
         
+        listOfAchievments.push(Achievement(now, _title, _description));
+        
         uint id = listOfAchievments.push(Achievement(now, _title, _description)) - 1;
-      
+        
         emit NewAchievement(id, listOfAchievments[id].date, listOfAchievments[id].title, listOfAchievments[id].description);
         
     }
@@ -28,6 +30,10 @@ contract AchieArchi {
         achieTitle = listOfAchievments[_id].title;
         achieDescription = listOfAchievments[_id].description;
 
+    }
+    
+    function getLengthOfList() public view returns(uint) {
+        return listOfAchievments.length;
     }
     
 }
