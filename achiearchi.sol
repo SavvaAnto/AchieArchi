06c3cc2a3c280c9592fc8c932c905a7fe5d7b262
w0ckeezz <39992389+w0ckeezz@users.noreply.github.com>
2018-12-19 22:51:38 +0300

    Simple Smart Contract for the appication
    addAchievement and getAchievement functions have been created.

---
+++ achiearchi.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.0;
+
+contract AchieArchi {
+    
+    event NewAchievement(uint achieId, uint achieDate, string achieTitle, string achieDescription);
+    
+    struct Achievement {
+        
+        uint date;
+        string title;
+        string description;
+        
+    }
+    
+    Achievement[] listOfAchievments;
+    
+    function addAchievement(string memory _title, string memory _description) public {
+        
+        uint id = listOfAchievments.push(Achievement(now, _title, _description)) - 1;
+      
+        emit NewAchievement(id, listOfAchievments[id].date, listOfAchievments[id].title, listOfAchievments[id].description);
+        
+    }
+    
+    function getAchievement(uint _id) public view returns(uint achieDate, string memory achieTitle, string memory achieDescription) {
+        
+        achieDate = listOfAchievments[_id].date;
+        achieTitle = listOfAchievments[_id].title;
+        achieDescription = listOfAchievments[_id].description;
+
+    }
+    
+}
